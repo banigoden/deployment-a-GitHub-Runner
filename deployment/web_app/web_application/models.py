5de86e71fd956f0a42557e575cb87bd98a05c3de
Denis Bandurin <banigoden@gmail.com>
2024-03-10 23:11:48 +0100

    added 2 vm's with prometheus

---
+++ deployment/web_app/web_application/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.urls import reverse
+
+#A model is the single, definitive source of information about your data.
+# It contains the essential fields and behaviors of the data you’re storing.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    second_name = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    email = models.EmailField()
+    date = models.DateField()
+    level = models.IntegerField(validators =[MinValueValidator(1), MaxValueValidator(5)])
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.second_name} - {self.department}"
---
+++ deployment/web_app/web_application/serializers.py
@@ -0,0 +1,9 @@
+
+from rest_framework import serializers
+from .models import Employee
+#Serializers allow you to convert complex data types, such as Django models, into Python data types
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = '__all__'
