587068d3540c8e037a9a20979d0488d617113877
Denis Bandurin <banigoden@gmail.com>
2024-03-24 15:14:50 +0100

    updated inventory file

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -1,56 +1,49 @@
-resource "tls_private_key" "ec2_keys" {
-  count     = length(var.instance_names)
+resource "tls_private_key" "ec2_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
 resource "aws_key_pair" "ssh_key" {
-  key_name   = "deployer-key"
-  public_key = public_key = tls_private_key.ec2_keys.public_key_openssh
-}
-
-
-resource "local_file" "ec2_files" {
-  count    = length(var.instance_names)
-  content  = tls_private_key.ec2_keys[count.index].private_key_pem
-  filename = "EC2-${var.instance_names[count.index]}"
+  key_name   = "EC2-${var.instance_name}"
+  public_key = tls_private_key.ec2_key.public_key_openssh
 }
 
 resource "local_sensitive_file" "pem_files" {
-  count                = length(var.instance_names)
-  filename             = "~/.ssh/EC2-${var.instance_names[count.index]}.pem"
-  file_permission      = "0600"
-  directory_permission = "0700"
-  content              = tls_private_key.ec2_keys[count.index].private_key_pem
+  filename        = "${path.module}./ansible/EC2-${var.instance_name}.pem"
+  file_permission = "0600"
+  # directory_permission = "0700"
+  content = tls_private_key.ec2_key.private_key_pem
 }
 
 # Create an AWS instance
 resource "aws_instance" "django" {
   ami                         = var.ami_number
-  instance_type               = "t2.micro"
-  key_name                    = "EC2-${var.instance_names[0]}.pem"
+  instance_type               = var.size
+  key_name                    = aws_key_pair.ssh_key.key_name
   vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
   subnet_id                   = aws_subnet.public_subnet.id
   associate_public_ip_address = true
 
-
-  provisioner "local-exec" {
-    command = "ansible-playbook -i '${aws_instance.django.public_ip},' -u ec2-user --private-key=${tls_private_key.ec2_key.ec2-django}.pem /Users/denisbandurin/Desktop/django_project/ansible/playbook.yml"
-  }
   tags = {
     Name = "web-production"
   }
-}
+  depends_on = [aws_instance.django]
 
-resource "aws_instance" "monitoring" {
-  ami                         = var.ami_number
-  instance_type               = "t2.micro"
-  key_name                    = "EC2-${var.instance_names[1]}.pem"
-  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
-  subnet_id                   = aws_subnet.public_subnet.id
-  associate_public_ip_address = true
+  #Because AWS instance needs some time to be ready for usage we will use below trick with remote-exec.
+  #As per documentation remote-exec waits for successful connection and only after this runs command.
+  provisioner "remote-exec" {
+    inline = ["sudo hostname"]
 
-  tags = {
-    Name = "monitoring"
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("${path.module}./ansible/EC2-django.pem")
+      host        = self.public_ip
+    }
   }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ${path.module}./ansible/inventory.tpl ${path.module}./ansible/playbook.yml --private-key ${path.module}./ansible/EC2-django.pem --user ec2-user"
+  }
+
 }
