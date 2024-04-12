99e6214ff2edf081c24c565eecfcc372b3e001b8
Denis Bandurin <banigoden@gmail.com>
2024-04-12 17:37:29 +0200

    Changed ansible code for github actions, but github runner configuration doesn't work Run configuration

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -9,10 +9,10 @@ resource "aws_key_pair" "ssh_key" {
 }
 
 resource "local_sensitive_file" "pem_files" {
-  filename        = "${path.module}./ansible/EC2-${var.instance_name}.pem"
-  file_permission = "0600"
-  # directory_permission = "0700"
-  content = tls_private_key.ec2_key.private_key_pem
+  filename             = "${path.module}./ansible/EC2-${var.instance_name}.pem"
+  file_permission      = "0600"
+  directory_permission = "0700"
+  content              = tls_private_key.ec2_key.private_key_pem
 }
 
 # Create an AWS instance
@@ -43,6 +43,10 @@ resource "aws_instance" "django" {
 resource "null_resource" "run_ansible" {
 
   depends_on = [aws_instance.django]
+
+  provisioner "local-exec" {
+    command = "sleep 20"
+  }
 
   provisioner "local-exec" {
     command = "ansible-playbook -i ${path.module}./ansible/inventory/ansible-inventory ${path.module}./ansible/playbook.yml --private-key ${path.module}./ansible/EC2-django.pem --user ec2-user"
